04f4bfa1056794ed21fea0b118fcc44a6c1322e1
Tim Tianyang Chen <soapcn@gmail.com>
2015-07-04 06:19:22 -0400

    fixed untracked files

--- dspic/nbproject/Package-C30_dsPIC33FJ256GP710A.bash
+++ dspic/nbproject/Package-C30_dsPIC33FJ256GP710A.bash
@@ -10,9 +10,9 @@ CND_CONF=C30_dsPIC33FJ256GP710A
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/PathRevB.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=PathRevB.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=pathrevb/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/dspic.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=dspic.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=dspic/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/pathrevb/bin
+makeDirectory ${TMPDIR}/dspic/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/pathrevb.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/dspic.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/pathrevb.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/dspic.tar *
 checkReturnCode
 
 # Cleanup
